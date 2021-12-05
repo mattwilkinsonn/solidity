bcf4606c83f106dea1a545107941e27c8de888c6
Matt Wilkinson <mattwilki17@gmail.com>
2021-12-05 18:05:24 -0500

    init

---
+++ contracts/FundMe.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.6 <0.9.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable {
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+        return priceFeed.version();
+    }
+}
